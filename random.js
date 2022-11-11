88b1ac8d114c5084b64e42902430eba26ca6e54a
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-11 13:18:01 +0100

    boutonRandom non fonctionnel

--- random.js
+++ random.js
@@ -1,15 +1,14 @@
 const bouton = document.getElementById("random");
-const NbApprenant = 16;
-let NumApprenantChoisi;
+let PrenomElu;
 const LesPrenoms = ['Arnaud', 'Delphine', 'Kevin', 'Johann', 'Terry',
- 'Pierre', 'Sebastien', 'Sokunthy', 'Stéphane', 'Jessie', 'Jean-Sebastien',
-  'Alexandre', 'Laure', 'Jean-Christophe', 'Sandrine', 'Jean-François', 'Arnaud'];
+    'Pierre', 'Sebastien', 'Sokunthy', 'Stéphane', 'Jessie', 'Jean-Sebastien',
+    'Alexandre', 'Laure', 'Jean-Christophe', 'Sandrine', 'Jean-François', 'Arnaud'];
+const NbApprenant = LesPrenoms.length;
 
 
-
-function Randomize(
-    NumApprenantChoisi = Math.random()*NbApprenant
-
-)
+function Randomize (){
+    PrenomElu = LesPrenoms[(Math.random() * NbApprenant)];
+    bouton.innerHTML = PrenomElu;
+}
 
 bouton.addEventListener('click', Randomize);
