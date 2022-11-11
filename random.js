8c725b2fcbdfd30116caddde22d33549b6d2a3d5
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-11 18:39:03 +0100

    randomize + timer non fonctionnel

--- random.js
+++ random.js
@@ -1,19 +1,23 @@
+/*-- Création des variables et constantes --*/
 const bouton = document.getElementById("random");
+const TextBouton = document.getElementById("TextRandomButton");
 let PrenomElu;
+/*-- Tableau comprenant les prenoms --*/
 const LesPrenoms = ['Arnaud', 'Delphine', 'Kevin', 'Johann', 'Terry',
     'Pierre', 'Sebastien', 'Sokunthy', 'Stéphane', 'Jessie', 'Jean-Sebastien',
-    'Alexandre', 'Laure', 'Jean-Christophe', 'Sandrine', 'Jean-François', 'Arnaud'];
+    'Alexandre', 'Laure', 'Jean-Christophe', 'Sandrine', 'Jean-François'];
+/*-- determination du nombre de prenom dans le tableau -*/
 const NbApprenant = LesPrenoms.length;
 
-
+/*-- fonction choisissant un prénom au hasard dans le tableau et l'affichant 3 sec --*/
 function Randomize (){
     PrenomElu = LesPrenoms[(Math.random() * NbApprenant)];
-    bouton.innerHTML = PrenomElu;
+    TextBouton.innerHTML = PrenomElu;
     setTimeout(ClearPrenom,3000);
-}
-
+};
+/*-- Fonction réinitialisant le bouton avec son "texte" d'origine --*/
 function ClearPrenom(){
-    bouton.innerHTML = "RANDOM APPRENANT";
-}
-
+    TextBouton.innerHTML = "RANDOM APPRENANT";
+};
+/*-- Declenchement de la fonction Randomize sur clic du bouton --*/
 bouton.addEventListener('click', Randomize);
