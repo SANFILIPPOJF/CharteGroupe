e0d5f901bd3a94c88d0686b3a868d66b78ab3652
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-13 20:17:31 +0100

    Essai de bouton random non fonctionnel

--- random.js
+++ random.js
@@ -1,23 +1,20 @@
-/*-- Création des variables et constantes --*/
-const bouton = document.getElementById("random");
-const TextBouton = document.getElementById("TextRandomButton");
-let PrenomElu;
-/*-- Tableau comprenant les prenoms --*/
+/*-- Création des variables et constantes et tableaux des prénoms--*/
+const bouton = document.querySelector("random");
+const texte = document.getElementById('resultat');
 const LesPrenoms = ['Arnaud', 'Delphine', 'Kevin', 'Johann', 'Terry',
     'Pierre', 'Sebastien', 'Sokunthy', 'Stéphane', 'Jessie', 'Jean-Sebastien',
     'Alexandre', 'Laure', 'Jean-Christophe', 'Sandrine', 'Jean-François'];
-/*-- determination du nombre de prenom dans le tableau -*/
+
 const NbApprenant = LesPrenoms.length;
+/*-- Declenchement de la fonction Randomize sur clic du bouton --*/
+bouton.addEventListener('click', ClearPrenom);
 
 /*-- fonction choisissant un prénom au hasard dans le tableau et l'affichant 3 sec --*/
 function Randomize (){
-    PrenomElu = LesPrenoms[(Math.random() * NbApprenant)];
-    TextBouton.innerHTML = PrenomElu;
-    setTimeout(ClearPrenom,3000);
-};
+    texte.innerHTML = LesPrenoms[Math.floor(Math.random() * NbApprenant)];
+    setTimeout(ClearPrenom(),3000);
+}
 /*-- Fonction réinitialisant le bouton avec son "texte" d'origine --*/
 function ClearPrenom(){
-    TextBouton.innerHTML = "RANDOM APPRENANT";
-};
-/*-- Declenchement de la fonction Randomize sur clic du bouton --*/
-bouton.addEventListener('click', Randomize);
+    texte.innerHTML = "Un suivant(e)";
+}
