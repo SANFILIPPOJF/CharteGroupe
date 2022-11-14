ae63aa0dfbbb9b0551ecfe242fa4c48222c635b2
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-14 10:49:34 +0100

    V1.0 random fonctionne git add .

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 /*-- Création des variables et constantes et tableaux des prénoms--*/
-/*const bouton = document.querySelector("random");*/
+const bouton = document.getElementById("random");
 const texte = document.getElementById('resultat');
 const LesPrenoms = ['Arnaud', 'Delphine', 'Kevin', 'Johann', 'Terry',
     'Pierre', 'Sebastien', 'Sokunthy', 'Stéphane', 'Jessie', 'Jean-Sebastien',
@@ -7,12 +7,12 @@ const LesPrenoms = ['Arnaud', 'Delphine', 'Kevin', 'Johann', 'Terry',
 
 const NbApprenant = LesPrenoms.length;
 /*-- Declenchement de la fonction Randomize sur clic du bouton --*/
-bouton.addEventListener('click', ClearPrenom);
+bouton.addEventListener('click', Randomize);
 
 /*-- fonction choisissant un prénom au hasard dans le tableau et l'affichant 3 sec --*/
 function Randomize (){
     texte.innerHTML = LesPrenoms[Math.floor(Math.random() * NbApprenant)];
-    setTimeout(ClearPrenom(),3000);
+    setTimeout(ClearPrenom,3000);
 }
 /*-- Fonction réinitialisant le bouton avec son "texte" d'origine --*/
 function ClearPrenom(){
